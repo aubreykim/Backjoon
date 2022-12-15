dcbb6506be5e6309ef808ca124ed0365285a4860
aubreykim <109501986+aubreykim@users.noreply.github.com>
2022-12-15 14:28:53 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 216 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main{
+	
+	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
+		int k = scan.nextInt();
+		int q = scan.nextInt();
+		int l = scan.nextInt();
+		int b = scan.nextInt();
+		int n = scan.nextInt();
+		int p = scan.nextInt();
+		
+		System.out.println(1-k);
+		System.out.println(1-q);
+		System.out.println(2-l);
+		System.out.println(2-b);
+		System.out.println(2-n);
+		System.out.println(8-p);
+
+	}
+}
