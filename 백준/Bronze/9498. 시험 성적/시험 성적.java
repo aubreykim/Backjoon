eff452decab2568cc62f8e01a9b07fe0437342c5
aubreykim <109501986+aubreykim@users.noreply.github.com>
2022-12-15 14:30:34 +0900

    [Bronze V] Title: 시험 성적, Time: 212 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main{
+	
+	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
+		int score = scan.nextInt();
+		String grade = "null";
+		if (score >= 90 && score <= 100) {
+			grade="A";
+		} else if (score >= 80) {
+			grade="B";
+		} else if (score >= 70) {
+			grade="C";
+		} else if (score >= 60) {
+			grade = "D";
+		} else {
+			grade = "F";
+		}
+		System.out.println(grade);
+		
+	}
+}
