a4d394c49c48af61ef6c031e2c09331db97b840d
aubreykim <109501986+aubreykim@users.noreply.github.com>
2022-12-15 14:28:30 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main{
+	
+	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
+		String id = scan.nextLine();
+		
+		if (id.equals(id)) {
+			System.out.println(id+"??!");
+		}
+	}
+}
