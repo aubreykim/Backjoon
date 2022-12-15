8672779445b8acfa49d72bd37eef8a86c7c9c77a
aubreykim <109501986+aubreykim@users.noreply.github.com>
2022-12-15 14:28:45 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main{
+	
+	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
+		int y = scan.nextInt()-543;
+		
+		System.out.println(y);
+		
+		
+	}
+}
