f7b66908f459d1959ff0ff7f33e56770b5e96d1a
aubreykim <109501986+aubreykim@users.noreply.github.com>
2023-01-24 14:25:23 +0900

    Update 순서쌍의 개수.java
    add error code

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -1,5 +1,19 @@
 class Solution {
     public int solution(int n) {
+        
+    /*
+        error code!
+        시간 초과와 실패 발생
+        
+        for (int i=1; i<= n; i++) {
+            for (int j=1; j<=n; j++){
+                if (i*j == n) {
+                    answer++;
+                
+            }
+        }
+    */
+        
         int answer = 0;
         
         for (int i=1; i<= n; i++) {
