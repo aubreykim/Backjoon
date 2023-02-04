66e28991e6acf4368e465e89a2e5e9dea3c9317d
aubreykim <109501986+aubreykim@users.noreply.github.com>
2023-02-04 15:30:08 +0900

    [level 0] Title: 모스부호 (1), Time: 0.40 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String letter) {
+        
+        Map<String, String> morseMap = new HashMap<>() {
+            {
+                put(".-", "a");
+                put("-...", "b");
+                put("-.-.", "c");
+                put("-..", "d");
+                put(".", "e");
+                put("..-.", "f");
+                put("--.", "g");
+                put("....", "h");
+                put("..", "i");
+                put(".---", "j");
+                put("-.-", "k");
+                put(".-..", "l");
+                put("--", "m");
+                put("-.", "n");
+                put("---", "o");
+                put(".--.", "p");
+                put("--.-", "q");
+                put(".-.", "r");
+                put("...", "s");
+                put("-", "t");
+                put("..-", "u");
+                put("...-", "v");
+                put(".--", "w");
+                put("-..-", "x");
+                put("-.--", "y");
+                put("--..", "z");
+            }
+        };
+       
+        String[] array =  letter.split(" ");
+        StringBuilder sb = new StringBuilder();
+        
+        for (String temp : array) {
+            sb.append(morseMap.get(temp));
+        }
+        return sb.toString();
+    }
+}
